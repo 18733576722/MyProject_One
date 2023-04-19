d807b178b1c00871e5954f0dcfbf650014aed033
马凯 <1653848506@qq.com>
2023-04-19 16:33:20 +0800

    页面布局

---
+++ FrontProject/src/layout/layout.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+</template>
+<script>
+</script>
+<style>
+html{
+  padding: 0;
+}
+body{
+  padding: 0;
+  margin: 0;
+}
+.el-header{
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    height: 100%;
+    /* line-height: 200px; */
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    /* line-height: 160px; */
+    height: 100%;
+  }
+</style>
